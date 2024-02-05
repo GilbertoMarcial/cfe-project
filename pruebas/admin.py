5fc3893a08cc494f81eb807a0efb95256f0ec427
Gilberto Marcial <gilberto.marcial@hotmail.com>
2024-02-05 00:15:32 -0600

    Creados y corregidos los modelos de Unidad y Trafo (falta Generador). Unidad y Trafo están correlacionados, cuando se agrega un Trafo, automáticamente se agrega un Equipo con sus campos respectivos.

--- pruebas/admin.py
+++ pruebas/admin.py
@@ -25,5 +25,66 @@ class AdminSubgerencia(admin.ModelAdmin):
         model = Subgerencia
 
 
+class AdminCentral(admin.ModelAdmin):
+    list_display = ['alias', 'name', 'subgerencia', 'create', 'modify', ]
+    list_display_links = None
+    list_filter = ['create', 'modify', ]
+    search_fields = ['alias', 'name', 'subgerencia__name', ]
+    ordering = ['name', ]
+
+    class Meta:
+        model = Central
+
+
+class AdminUnidad(admin.ModelAdmin):
+    list_display = ['name', 'central', 'create', 'modify', ]
+    list_display_links = None
+    list_filter = ['create', 'modify', ]
+    search_fields = ['name', 'central__name', ]
+    ordering = ['name', ]
+
+    class Meta:
+        model = Unidad
+
+
+class AdminEquipo(admin.ModelAdmin):
+    def get_unidades_names(self, obj):
+        return ', '.join([unidad.name for unidad in obj.unidades.all()])
+
+    get_unidades_names.short_description = 'Unidades'
+
+    list_display = ['name', 'n_serie', 'ano_puesta_servicio', 'condicion_operacion', 'razon_fuera_servicio', 'problematica_operativa', 'indice_condicion', 'observaciones', 'get_unidades_names', 'create', 'modify', ]
+    filter_horizontal = ('unidades',)
+    list_display_links = None
+    list_filter = ['create', 'modify', ]
+    search_fields = ['alias', 'name', 'unidad__name', ]
+    ordering = ['name', ]
+
+    class Meta:
+        model = Equipo
+
+    def display_unidades(self, obj):
+        return ", ".join([unidad.nombre for unidad in obj.unidades.all()])
+
+
+class AdminTrafo(admin.ModelAdmin):
+    list_display = ['name', 'n_serie', 'ano_puesta_servicio', 'condicion_operacion', 'razon_fuera_servicio', 'problematica_operativa', 'indice_condicion', 'observaciones', 'n_devanados', 'kv_nominal_at', 'kv_nominal_bt', 'tension_kv', 'elevacion_temperatura', 'create', 'modify', ]
+    filter_horizontal = ('unidades',)
+    list_display_links = None
+    list_filter = ['create', 'modify', ]
+    search_fields = ['name', 'unidad__name', ]
+    ordering = ['name', ]
+
+    class Meta:
+        model = Trafo
+
+    def display_unidades(self, obj):
+        return ", ".join([unidad.nombre for unidad in obj.unidades.all()])
+
+
 admin.site.register(EPS, AdminEPS)
 admin.site.register(Subgerencia, AdminSubgerencia)
+admin.site.register(Central, AdminCentral)
+admin.site.register(Unidad, AdminUnidad)
+admin.site.register(Equipo, AdminEquipo)
+admin.site.register(Trafo, AdminTrafo)
--- pruebas/models.py
+++ pruebas/models.py
@@ -31,6 +31,10 @@ class Subgerencia(models.Model):
         verbose_name_plural = 'Subgerencias'
         ordering = ['name', ]
 
+    @property
+    def get_eps(self):
+        return self.eps.name
+
 
 class Central(models.Model):
     alias = models.CharField(max_length=50, verbose_name='Clave', unique=True)
@@ -45,4 +49,65 @@ class Central(models.Model):
     class Meta:
         verbose_name = 'Central'
         verbose_name_plural = 'Centrales'
+        ordering = ['name', ]
+
+    @property
+    def get_subgerencia(self):
+        return self.subgerencia.name
+
+
+class Unidad(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Nombre')
+    create = models.DateTimeField(auto_now_add=True, verbose_name='Creado')
+    modify = models.DateTimeField(auto_now=True, verbose_name='Modificado')
+    central = models.ForeignKey(Central, on_delete=models.CASCADE, verbose_name='Central')
+
+    def __str__(self):
+        return '%s [%s]' % (self.name, self.central.name)
+
+    class Meta:
+        verbose_name = 'Unidad'
+        verbose_name_plural = 'Unidades'
+        ordering = ['name', ]
+
+    @property
+    def get_central(self):
+        return self.central.name
+
+
+class Equipo(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Nombre')
+    n_serie = models.CharField(max_length=50, verbose_name='Número de Serie', unique=True)
+    ano_puesta_servicio = models.IntegerField(verbose_name='Año de Puesta en Servicio')
+    condicion_operacion = models.TextField(verbose_name='Condición de Operación', blank=True)
+    razon_fuera_servicio = models.TextField(verbose_name='Razón Fuera de Servicio', blank=True)
+    problematica_operativa = models.TextField(verbose_name='Problemática Operativa', blank=True)
+    indice_condicion = models.TextField(verbose_name='Índice de Condición', blank=True)
+    observaciones = models.TextField(verbose_name='Observaciones', blank=True)
+    create = models.DateTimeField(auto_now_add=True, verbose_name='Creado')
+    modify = models.DateTimeField(auto_now=True, verbose_name='Modificado')
+    unidades = models.ManyToManyField(Unidad, verbose_name='Unidades')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Equipo'
+        verbose_name_plural = 'Equipos'
+        ordering = ['name', ]
+
+
+class Trafo(Equipo):
+    n_devanados = models.IntegerField(verbose_name='Número de Devanados')
+    kv_nominal_at = models.FloatField(verbose_name='KV Nominal AT')
+    kv_nominal_bt = models.FloatField(verbose_name='KV Nominal BT')
+    tension_kv = models.FloatField(verbose_name='Tensión KV')
+    elevacion_temperatura = models.FloatField(verbose_name='Elevación de Temperatura')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Trafo'
+        verbose_name_plural = 'Trafos'
         ordering = ['name', ]
