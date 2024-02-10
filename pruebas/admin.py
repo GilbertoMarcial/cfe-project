f1d9794e9f6191b09f4a4c29220d6e560084b344
Gilberto Marcial <gilberto.marcial@hotmail.com>
2024-02-09 18:48:53 -0600

    Se agregaron campos a trafo. Se creo Prueba Eléctrica de Trafos, Prueba Físico Químicas y Prueba Cromatografía de Gases. Falta generar todas las dependencias de Prueba Eléctrica de Trafos.

--- pruebas/admin.py
+++ pruebas/admin.py
@@ -5,7 +5,6 @@ from django.http import HttpResponse
 
 class AdminEPS(admin.ModelAdmin):
     list_display = ['name', 'create', 'modify', ]
-    list_display_links = None
     list_filter = ['create', 'modify', ]
     search_fields = ['name', ]
     ordering = ['name', ]
@@ -16,7 +15,6 @@ class AdminEPS(admin.ModelAdmin):
 
 class AdminSubgerencia(admin.ModelAdmin):
     list_display = ['name', 'eps', 'create', 'modify', ]
-    list_display_links = None
     list_filter = ['create', 'modify', ]
     search_fields = ['name', 'eps__name', ]
     ordering = ['name', ]
@@ -27,7 +25,6 @@ class AdminSubgerencia(admin.ModelAdmin):
 
 class AdminCentral(admin.ModelAdmin):
     list_display = ['alias', 'name', 'subgerencia', 'create', 'modify', ]
-    list_display_links = None
     list_filter = ['create', 'modify', ]
     search_fields = ['alias', 'name', 'subgerencia__name', ]
     ordering = ['name', ]
@@ -38,7 +35,6 @@ class AdminCentral(admin.ModelAdmin):
 
 class AdminUnidad(admin.ModelAdmin):
     list_display = ['name', 'central', 'create', 'modify', ]
-    list_display_links = None
     list_filter = ['create', 'modify', ]
     search_fields = ['name', 'central__name', ]
     ordering = ['name', ]
@@ -53,9 +49,10 @@ class AdminEquipo(admin.ModelAdmin):
 
     get_unidades_names.short_description = 'Unidades'
 
-    list_display = ['name', 'n_serie', 'ano_puesta_servicio', 'condicion_operacion', 'razon_fuera_servicio', 'problematica_operativa', 'indice_condicion', 'observaciones', 'get_unidades_names', 'create', 'modify', ]
+    list_display = ['name', 'n_serie', 'ano_puesta_servicio', 'condicion_operacion', 'razon_fuera_servicio',
+                    'problematica_operativa', 'indice_condicion', 'observaciones', 'get_unidades_names', 'create',
+                    'modify', ]
     filter_horizontal = ('unidades',)
-    list_display_links = None
     list_filter = ['create', 'modify', ]
     search_fields = ['alias', 'name', 'unidad__name', ]
     ordering = ['name', ]
@@ -68,9 +65,12 @@ class AdminEquipo(admin.ModelAdmin):
 
 
 class AdminTrafo(admin.ModelAdmin):
-    list_display = ['name', 'n_serie', 'ano_puesta_servicio', 'condicion_operacion', 'razon_fuera_servicio', 'problematica_operativa', 'indice_condicion', 'observaciones', 'n_devanados', 'kv_nominal_at', 'kv_nominal_bt', 'tension_kv', 'elevacion_temperatura', 'create', 'modify', ]
-    filter_horizontal = ('unidades',)
-    list_display_links = None
+    list_display = ['name', 'n_serie', 'ano_puesta_servicio', 'condicion_operacion', 'razon_fuera_servicio',
+                    'problematica_operativa', 'indice_condicion', 'observaciones', 'n_devanados', 'kv_nominal_at',
+                    'kv_nominal_bt', 'tension_kv', 'elevacion_temperatura', 'tipo_servicio', 'configuracion', 'fase',
+                    'conexion', 'enfriamiento', 'mva', 'impedancia', 'marca', 'estatus_revision',
+                    'fecha_revision_aprobada', 'create', 'modify', ]
+    filter_horizontal = ('unidades', )
     list_filter = ['create', 'modify', ]
     search_fields = ['name', 'unidad__name', ]
     ordering = ['name', ]
@@ -82,9 +82,138 @@ class AdminTrafo(admin.ModelAdmin):
         return ", ".join([unidad.nombre for unidad in obj.unidades.all()])
 
 
+class AdminTipoServicio(admin.ModelAdmin):
+    list_display = ['name', ]
+    search_fields = ['name', ]
+    ordering = ['name', ]
+
+    class Meta:
+        model = TipoServicio
+
+
+class AdminConfiguracion(admin.ModelAdmin):
+    list_display = ['name', ]
+    search_fields = ['name', ]
+    ordering = ['name', ]
+
+    class Meta:
+        model = Configuracion
+
+
+class AdminFase(admin.ModelAdmin):
+    list_display = ['name', ]
+    search_fields = ['name', ]
+    ordering = ['name', ]
+
+    class Meta:
+        model = Fase
+
+
+class AdminConexion(admin.ModelAdmin):
+    list_display = ['name', ]
+    search_fields = ['name', ]
+    ordering = ['name', ]
+
+    class Meta:
+        model = Conexion
+
+
+class AdminEnfriamiento(admin.ModelAdmin):
+    list_display = ['name', ]
+    search_fields = ['name', ]
+    ordering = ['name', ]
+
+    class Meta:
+        model = Enfriamiento
+
+
+class AdminMVA(admin.ModelAdmin):
+    list_display = ['mva_1', 'mva_2', 'mva_3', 'mva_4', ]
+
+    class Meta:
+        model = MVA
+
+
+class AdminImpedancia(admin.ModelAdmin):
+    list_display = ['z_1', 'z_2', 'z_3', 'z_4', ]
+
+    class Meta:
+        model = Impedancia
+
+
+class AdminMarca(admin.ModelAdmin):
+    list_display = ['name', ]
+    search_fields = ['name', ]
+    ordering = ['name', ]
+
+    class Meta:
+        model = Marca
+
+
+class AdminPrueba(admin.ModelAdmin):
+    list_display = ['matricula', 'trafo', 'get_unidades', 'get_central', 'get_subgerencia', 'get_eps', 'create',
+                    'modify', ]
+    list_filter = ['create', 'modify', ]
+    search_fields = ['matricula', ]
+    ordering = ['matricula', ]
+
+    class Meta:
+        model = Prueba
+
+
+class AdminElectricaTrafo(admin.ModelAdmin):
+    list_display = ['prueba', 'fecha_prueba', 'procesado_por', 'get_trafo', 'get_unidades', 'get_central',
+                    'get_subgerencia', 'get_eps', 'create', 'modify', ]
+    list_filter = ['create', 'modify', ]
+    search_fields = ['prueba__matricula', ]
+    ordering = ['prueba__matricula', ]
+
+    class Meta:
+        model = ElectricaTrafo
+
+
+# class AdminRespuestaFrecuencia(admin.ModelAdmin):
+#     list_display = ['matricula', 'zona_1', 'zona_2', 'zona_3', 'zona_4', ]
+#     search_fields = ['electrica_trafo__prueba__matricula', ]
+#     ordering = ['electrica_trafo__prueba__matricula', ]
+#
+#     def matricula(self, obj):
+#         return obj.prueba.matricula
+#
+#     class Meta:
+#         model = RespuestaFrecuencia
+
+
+class AdminFisicoQuimica(admin.ModelAdmin):
+    list_display = ['prueba', 'fecha_prueba', 'exploratoria_aceite', 'analisis_gases_disueltos', 'contenido_humedad',
+                    'fp_liquido_25c', 'fp_liquido_100c', 'contenido_inhibidor_oxidacion', 'compuestos_furanicos',
+                    'rigidez_dielectrica', 'factor_potencia', 'tension_interfacial', 'color', 'examinacion_visual',
+                    'nivel_neutralizacion', 'contenido_agua', 'contenido_inhibidor_oxidacion_especifico',
+                    'azufre_corrosivo', 'humedad_resiudal', 'get_trafo', 'get_unidades', 'get_central',
+                    'get_subgerencia', 'get_eps', 'create', 'modify', ]
+    list_filter = ['create', 'modify', ]
+    search_fields = ['prueba__matricula', ]
+    ordering = ['prueba__matricula', ]
+
+    class Meta:
+        model = FisicoQuimica
+
+
 admin.site.register(EPS, AdminEPS)
 admin.site.register(Subgerencia, AdminSubgerencia)
 admin.site.register(Central, AdminCentral)
 admin.site.register(Unidad, AdminUnidad)
 admin.site.register(Equipo, AdminEquipo)
 admin.site.register(Trafo, AdminTrafo)
+admin.site.register(TipoServicio, AdminTipoServicio)
+admin.site.register(Configuracion, AdminConfiguracion)
+admin.site.register(Fase, AdminFase)
+admin.site.register(Conexion, AdminConexion)
+admin.site.register(Enfriamiento, AdminEnfriamiento)
+admin.site.register(MVA, AdminMVA)
+admin.site.register(Impedancia, AdminImpedancia)
+admin.site.register(Marca, AdminMarca)
+admin.site.register(Prueba, AdminPrueba)
+admin.site.register(ElectricaTrafo, AdminElectricaTrafo)
+# admin.site.register(RespuestaFrecuencia, AdminRespuestaFrecuencia)
+admin.site.register(FisicoQuimica, AdminFisicoQuimica)
--- pruebas/models.py
+++ pruebas/models.py
@@ -86,6 +86,8 @@ class Equipo(models.Model):
     observaciones = models.TextField(verbose_name='Observaciones', blank=True)
     create = models.DateTimeField(auto_now_add=True, verbose_name='Creado')
     modify = models.DateTimeField(auto_now=True, verbose_name='Modificado')
+
+    # Un equipo puede pertenecer a 1 o más unidades
     unidades = models.ManyToManyField(Unidad, verbose_name='Unidades')
 
     def __str__(self):
@@ -97,12 +99,31 @@ class Equipo(models.Model):
         ordering = ['name', ]
 
 
+# Modelos para Trafo y atributos relacionados
 class Trafo(Equipo):
+    # Options
+    A = '1'
+    P = '2'
+    ESTATUS_CHOICES = (
+        (A, 'Aprobada'),
+        (P, 'Pendiente'),
+    )
+
     n_devanados = models.IntegerField(verbose_name='Número de Devanados')
     kv_nominal_at = models.FloatField(verbose_name='KV Nominal AT')
     kv_nominal_bt = models.FloatField(verbose_name='KV Nominal BT')
     tension_kv = models.FloatField(verbose_name='Tensión KV')
     elevacion_temperatura = models.FloatField(verbose_name='Elevación de Temperatura')
+    estatus_revision = models.CharField(verbose_name='Estatus de Revisión', choices=ESTATUS_CHOICES, default=P)
+    fecha_revision_aprobada = models.DateField(verbose_name='Fecha de Revisión Aprobada', blank=True, null=True)
+    tipo_servicio = models.ForeignKey('TipoServicio', on_delete=models.CASCADE, verbose_name='Tipo de Servicio')
+    configuracion = models.ForeignKey('Configuracion', on_delete=models.CASCADE, verbose_name='Configuración')
+    fase = models.ForeignKey('Fase', on_delete=models.CASCADE, verbose_name='Fase')
+    conexion = models.ForeignKey('Conexion', on_delete=models.CASCADE, verbose_name='Conexión')
+    enfriamiento = models.ForeignKey('Enfriamiento', on_delete=models.CASCADE, verbose_name='Enfriamiento')
+    mva = models.ForeignKey('MVA', on_delete=models.CASCADE, verbose_name='MVA')
+    impedancia = models.ForeignKey('Impedancia', on_delete=models.CASCADE, verbose_name='Impedancia')
+    marca = models.ForeignKey('Marca', on_delete=models.CASCADE, verbose_name='Marca')
 
     def __str__(self):
         return self.name
@@ -111,3 +132,440 @@ class Trafo(Equipo):
         verbose_name = 'Trafo'
         verbose_name_plural = 'Trafos'
         ordering = ['name', ]
+
+
+class TipoServicio(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Nombre', unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Tipo de Servicio'
+        verbose_name_plural = 'Tipos de Servicio'
+        ordering = ['name', ]
+
+
+class Configuracion(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Nombre', unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Configuración'
+        verbose_name_plural = 'Configuraciones'
+        ordering = ['name', ]
+
+
+class Fase(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Nombre', unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Fase'
+        verbose_name_plural = 'Fases'
+        ordering = ['name', ]
+
+
+class Conexion(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Nombre', unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Conexión'
+        verbose_name_plural = 'Conexiones'
+        ordering = ['name', ]
+
+
+class Enfriamiento(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Nombre', unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Enfriamiento'
+        verbose_name_plural = 'Enfriamientos'
+        ordering = ['name', ]
+
+
+class MVA(models.Model):
+    mva_1 = models.FloatField(verbose_name='MVA 1', default=0)
+    mva_2 = models.FloatField(verbose_name='MVA 2', default=0)
+    mva_3 = models.FloatField(verbose_name='MVA 3', default=0)
+    mva_4 = models.FloatField(verbose_name='MVA 4', default=0)
+
+    def __str__(self):
+        return '%s / %s / %s / %s ' % (self.mva_1, self.mva_2, self.mva_3, self.mva_4)
+
+    class Meta:
+        verbose_name = 'MVA'
+        verbose_name_plural = 'MVA'
+
+
+class Impedancia(models.Model):
+    z_1 = models.FloatField(verbose_name='Z 1', default=0)
+    z_2 = models.FloatField(verbose_name='Z 2', default=0)
+    z_3 = models.FloatField(verbose_name='Z 3', default=0)
+    z_4 = models.FloatField(verbose_name='Z 4', default=0)
+
+    def __str__(self):
+        return '%s / %s / %s / %s ' % (self.z_1, self.z_2, self.z_3, self.z_4)
+
+    class Meta:
+        verbose_name = 'Impedancia'
+        verbose_name_plural = 'Impedancias'
+
+
+class Marca(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Nombre', unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Marca'
+        verbose_name_plural = 'Marcas'
+        ordering = ['name', ]
+
+
+# Modelos para Generador y atributos relacionados
+# class Generador(Equipo):
+#     pass
+#
+#     def __str__(self):
+#         return self.name
+#
+#     class Meta:
+#         verbose_name = 'Generador'
+#         verbose_name_plural = 'Generadores'
+#         ordering = ['name', ]
+
+
+# Modelos para Pruebas
+class Prueba(models.Model):
+    matricula = models.CharField(max_length=50, verbose_name='Matrícula', unique=True)
+    create = models.DateTimeField(auto_now_add=True, verbose_name='Creado')
+    modify = models.DateTimeField(auto_now=True, verbose_name='Modificado')
+    trafo = models.ForeignKey(Trafo, on_delete=models.CASCADE, verbose_name='Trafo')
+
+    def __str__(self):
+        return self.matricula
+
+    class Meta:
+        verbose_name = 'Prueba'
+        verbose_name_plural = 'Pruebas'
+        ordering = ['matricula', ]
+
+    @property
+    def get_trafo(self):
+        return self.trafo.name
+
+    @property
+    def get_unidades(self):
+        return ", ".join([unidad.name for unidad in self.trafo.unidades.all()])
+
+    @property
+    def get_central(self):
+        return self.trafo.unidades.first().central.name
+
+    @property
+    def get_subgerencia(self):
+        return self.trafo.unidades.first().central.subgerencia.name
+
+    @property
+    def get_eps(self):
+        return self.trafo.unidades.first().central.subgerencia.eps.name
+
+
+# Prueba Eléctrica de Trafos y atributos relacionados
+class ElectricaTrafo(models.Model):
+    fecha_prueba = models.DateField(verbose_name='Fecha de Prueba')
+    procesado_por = models.CharField(max_length=255, verbose_name='Procesado por', unique=True)
+    create = models.DateTimeField(auto_now_add=True, verbose_name='Creado')
+    modify = models.DateTimeField(auto_now=True, verbose_name='Modificado')
+    prueba = models.ForeignKey(Prueba, on_delete=models.CASCADE, verbose_name='Prueba')
+
+    def __str__(self):
+        return self.prueba.matricula
+
+    class Meta:
+        verbose_name = 'Prueba Eléctrica de Trafos'
+        verbose_name_plural = 'Pruebas Eléctricas de Trafos'
+        ordering = ['fecha_prueba', 'prueba__matricula', ]
+
+    @property
+    def get_prueba(self):
+        return self.prueba.matricula
+
+    @property
+    def get_trafo(self):
+        return self.prueba.trafo.name
+
+    @property
+    def get_unidades(self):
+        return ", ".join([unidad.name for unidad in self.prueba.trafo.unidades.all()])
+
+    @property
+    def get_central(self):
+        return self.prueba.trafo.unidades.first().central.name
+
+    @property
+    def get_subgerencia(self):
+        return self.prueba.trafo.unidades.first().central.subgerencia.name
+
+    @property
+    def get_eps(self):
+        return self.prueba.trafo.unidades.first().central.subgerencia.eps.name
+
+
+# class RespuestaFrecuencia(models.Model):
+#     zona_1 = models.FloatField(verbose_name='Zona 1')
+#     zona_2 = models.FloatField(verbose_name='Zona 2')
+#     zona_3 = models.FloatField(verbose_name='Zona 3')
+#     zona_4 = models.FloatField(verbose_name='Zona 4')
+#     comentarios = models.TextField(verbose_name='Comentarios', blank=True)
+#
+#     electrica_trafo = models.ForeignKey(ElectricaTrafo, on_delete=models.CASCADE, verbose_name='Prueba Eléctrica de Trafos')
+#
+#     def __str__(self):
+#         return 'Zona 1: %s, Zona 2: %s, Zona 3: %s, Zona 4: %s' % (self.zona_1, self.zona_2, self.zona_3, self.zona_4)
+#
+#     class Meta:
+#         verbose_name = 'Respuesta de Frecuencia'
+#         verbose_name_plural = 'Respuestas de Frecuencia'
+#
+#     @property
+#     def get_matricula(self):
+#         return self.electrica_trafo.prueba.matricula
+
+
+# Prueba Cromatografía de gases y atributos relacionados
+# class EstatusPrueba(models.Model):
+#     name = models.CharField(max_length=50, verbose_name='Nombre', unique=True)
+#
+#     def __str__(self):
+#         return self.name
+#
+#     class Meta:
+#         verbose_name = 'Estatus de Prueba'
+#         verbose_name_plural = 'Estatus de Pruebas'
+#         ordering = ['name', ]
+#
+#
+# class CromatografiaGases(models.Model):
+#     estatus_prueba = models.ForeignKey(EstatusPrueba, on_delete=models.CASCADE, verbose_name='Estatus de Prueba')
+#     prueba = models.ForeignKey(Prueba, on_delete=models.CASCADE, verbose_name='Prueba')
+#
+#     def __str__(self):
+#         return self.prueba.matricula
+#
+#     class Meta:
+#         verbose_name = 'Cromatografía de Gases'
+#         verbose_name_plural = 'Cromatografías de Gases'
+#         ordering = ['prueba__matricula', ]
+#
+#
+# class CromatografiaGasesRelacionO2N2(models.Model):
+#     edad = models.FloatField(verbose_name='Edad', null=True, blank=True)
+#     o2 = models.FloatField(verbose_name='O2', null=True, blank=True)
+#     n2 = models.FloatField(verbose_name='N2', null=True, blank=True)
+#     o2entren2 = models.FloatField(verbose_name='O2 entre N2', null=True, blank=True)
+#     h2 = models.FloatField(verbose_name='H2', null=True, blank=True)
+#     ch4 = models.FloatField(verbose_name='CH4', null=True, blank=True)
+#     co = models.FloatField(verbose_name='CO', null=True, blank=True)
+#     c2h6 = models.FloatField(verbose_name='C2H6', null=True, blank=True)
+#     co2 = models.FloatField(verbose_name='CO2', null=True, blank=True)
+#     c2h4 = models.FloatField(verbose_name='C2H4', null=True, blank=True)
+#     c2h2 = models.FloatField(verbose_name='C2H2', null=True, blank=True)
+#     total_gc = models.FloatField(verbose_name='Total GC', null=True, blank=True)
+#     p_incremento = models.FloatField(verbose_name='% Incremento', null=True, blank=True)
+#     p_tgc_referencia = models.FloatField(verbose_name='% TGC Referencia', null=True, blank=True)
+#     p_tgc_ultimo = models.FloatField(verbose_name='% TGC Último', null=True, blank=True)
+#     diferencial = models.FloatField(verbose_name='Diferencial', null=True, blank=True)
+#     absoluto = models.FloatField(verbose_name='Absoluto', null=True, blank=True)
+#     cromatografia_gases = models.ForeignKey(CromatografiaGases, on_delete=models.CASCADE, verbose_name='Cromatografía de Gases')
+#     create = models.DateTimeField(auto_now_add=True, verbose_name='Creado')
+#     modify = models.DateTimeField(auto_now=True, verbose_name='Modificado')
+#
+#     def __str__(self):
+#         return self.cromatografia_gases.prueba.matricula
+#
+#     class Meta:
+#         verbose_name = 'Relación O2/N2'
+#         verbose_name_plural = 'Relaciones O2/N2'
+#         ordering = ['cromatografia_gases__prueba__matricula', ]
+#
+#     @property
+#     def get_prueba(self):
+#         return self.cromatografia_gases.prueba.matricula
+#
+#     @property
+#     def get_trafo(self):
+#         return self.cromatografia_gases.prueba.trafo.name
+#
+#     @property
+#     def get_unidades(self):
+#         return ", ".join([unidad.name for unidad in self.cromatografia_gases.prueba.trafo.unidades.all()])
+#
+#     @property
+#     def get_central(self):
+#         return self.cromatografia_gases.prueba.trafo.unidades.first().central.name
+#
+#     @property
+#     def get_subgerencia(self):
+#         return self.cromatografia_gases.prueba.trafo.unidades.first().central.subgerencia.name
+#
+#     @property
+#     def get_eps(self):
+#         return self.cromatografia_gases.prueba.trafo.unidades.first().central.subgerencia.eps.name
+#
+#
+# class Diferencial_O2N2(models.Model):
+#     h2 = models.FloatField(verbose_name='H2', null=True, blank=True)
+#     ch4 = models.FloatField(verbose_name='CH4', null=True, blank=True)
+#     co = models.FloatField(verbose_name='CO', null=True, blank=True)
+#     c2h6 = models.FloatField(verbose_name='C2H6', null=True, blank=True)
+#     co2 = models.FloatField(verbose_name='CO2', null=True, blank=True)
+#     c2h4 = models.FloatField(verbose_name='C2H4', null=True, blank=True)
+#     c2h2 = models.FloatField(verbose_name='C2H2', null=True, blank=True)
+#     cromatografia_gases = models.ForeignKey(CromatografiaGases, on_delete=models.CASCADE, verbose_name='Cromatografía de Gases')
+#     create = models.DateTimeField(auto_now_add=True, verbose_name='Creado')
+#     modify = models.DateTimeField(auto_now=True, verbose_name='Modificado')
+#
+#     def __str__(self):
+#         return self.cromatografia_gases.prueba.matricula
+#
+#     class Meta:
+#         verbose_name = 'Diferencial O2/N2'
+#         verbose_name_plural = 'Diferenciales O2/N2'
+#         ordering = ['cromatografia_gases__prueba__matricula', ]
+#
+#     @property
+#     def get_prueba(self):
+#         return self.cromatografia_gases.prueba.matricula
+#
+#     @property
+#     def get_trafo(self):
+#         return self.cromatografia_gases.prueba.trafo.name
+#
+#     @property
+#     def get_unidades(self):
+#         return ", ".join([unidad.name for unidad in self.cromatografia_gases.prueba.trafo.unidades.all()])
+#
+#     @property
+#     def get_central(self):
+#         return self.cromatografia_gases.prueba.trafo.unidades.first().central.name
+#
+#     @property
+#     def get_subgerencia(self):
+#         return self.cromatografia_gases.prueba.trafo.unidades.first().central.subgerencia.name
+#
+#     @property
+#     def get_eps(self):
+#         return self.cromatografia_gases.prueba.trafo.unidades.first().central.subgerencia.eps.name
+#
+#
+# class VelocidadEntrePruebas(models.Model):
+#     meses_entre_pruebas = models.FloatField(verbose_name='Meses entre Pruebas', null=True, blank=True)
+#     h2 = models.FloatField(verbose_name='H2', null=True, blank=True)
+#     ch4 = models.FloatField(verbose_name='CH4', null=True, blank=True)
+#     co = models.FloatField(verbose_name='CO', null=True, blank=True)
+#     c2h6 = models.FloatField(verbose_name='C2H6', null=True, blank=True)
+#     co2 = models.FloatField(verbose_name='CO2', null=True, blank=True)
+#     c2h4 = models.FloatField(verbose_name='C2H4', null=True, blank=True)
+#     c2h2 = models.FloatField(verbose_name='C2H2', null=True, blank=True)
+#
+#     def __str__(self):
+#         return self.cromatografia_gases.prueba.matricula
+#
+#     class Meta:
+#         verbose_name = 'Velocidad entre Pruebas'
+#         verbose_name_plural = 'Velocidades entre Pruebas'
+#         ordering = ['cromatografia_gases__prueba__matricula', ]
+#
+#     @property
+#     def get_prueba(self):
+#         return self.cromatografia_gases.prueba.matricula
+#
+#     @property
+#     def get_trafo(self):
+#         return self.cromatografia_gases.prueba.trafo.name
+#
+#     @property
+#     def get_unidades(self):
+#         return ", ".join([unidad.name for unidad in self.cromatografia_gases.prueba.trafo.unidades.all()])
+#
+#     @property
+#     def get_central(self):
+#         return self.cromatografia_gases.prueba.trafo.unidades.first().central.name
+#
+#     @property
+#     def get_subgerencia(self):
+#         return self.cromatografia_gases.prueba.trafo.unidades.first().central.subgerencia.name
+#
+#     @property
+#     def get_eps(self):
+#         return self.cromatografia_gases.prueba.trafo.unidades.first().central.subgerencia.eps.name
+
+
+# Prueba Físico Químicas y atributos relacionados
+class FisicoQuimica(models.Model):
+    fecha_prueba = models.DateField(verbose_name='Fecha de Prueba')
+    exploratoria_aceite = models.FloatField(verbose_name='Prueba Exploratoria de Aceite')
+    analisis_gases_disueltos = models.FloatField(verbose_name='Análisis de Gases Disueltos')
+    contenido_humedad = models.FloatField(verbose_name='Contenido de Humedad')
+    fp_liquido_25c = models.FloatField(verbose_name='FP Líquido a 25°C')
+    fp_liquido_100c = models.FloatField(verbose_name='PF Líquido a 100°C')
+    contenido_inhibidor_oxidacion = models.FloatField(verbose_name='Contenido de Inhibidor de Oxidación')
+    compuestos_furanicos = models.FloatField(verbose_name='Compuestos Furánicos')
+    rigidez_dielectrica = models.FloatField(verbose_name='Rigidez Dieléctrica')
+    factor_potencia = models.FloatField(verbose_name='Factor de Potencia')
+    tension_interfacial = models.FloatField(verbose_name='Tensión Interfacial')
+    color = models.FloatField(verbose_name='Color')
+    examinacion_visual = models.TextField(verbose_name='Examinación Visual', blank=True)
+    nivel_neutralizacion = models.FloatField(verbose_name='Nivel de Neutralización')
+    contenido_agua = models.FloatField(verbose_name='Contenido de Agua')
+    contenido_inhibidor_oxidacion_especifico = models.FloatField(verbose_name='Contenido de Inhibidor de Oxidación Específico')
+    azufre_corrosivo = models.FloatField(verbose_name='Azufre Corrosivo')
+    humedad_resiudal = models.FloatField(verbose_name='Humedad Residual')
+    create = models.DateTimeField(auto_now_add=True, verbose_name='Creado')
+    modify = models.DateTimeField(auto_now=True, verbose_name='Modificado')
+    prueba = models.ForeignKey(Prueba, on_delete=models.CASCADE, verbose_name='Prueba')
+
+    def __str__(self):
+        return self.prueba.matricula
+
+    class Meta:
+        verbose_name = 'Prueba Físico Química'
+        verbose_name_plural = 'Pruebas Físico Químicas'
+        ordering = ['fecha_prueba', 'prueba__matricula', 'prueba__trafo', 'prueba__trafo__unidades', 'prueba__trafo__unidades__central', ]
+
+    @property
+    def get_prueba(self):
+        return self.prueba.matricula
+
+    @property
+    def get_trafo(self):
+        return self.prueba.trafo.name
+
+    @property
+    def get_unidades(self):
+        return ", ".join([unidad.name for unidad in self.prueba.trafo.unidades.all()])
+
+    @property
+    def get_central(self):
+        return self.prueba.trafo.unidades.first().central.name
+
+    @property
+    def get_subgerencia(self):
+        return self.prueba.trafo.unidades.first().central.subgerencia.name
+
+    @property
+    def get_eps(self):
+        return self.prueba.trafo.unidades.first().central.subgerencia.eps.name
